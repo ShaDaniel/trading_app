365c4356b5ec675115acbe21ea85f3a2ec7af57a
Your Name <you@example.com>
2020-11-22 14:38:53 +0300

    2 sided listings done

--- lib/feed.dart
+++ lib/feed.dart
@@ -34,50 +34,84 @@ class _FeedListState extends State<FeedList> {
   }
 
   Widget _buildAdvertisements() {
-    return ListView.builder(
-        itemExtent: 300,
-        itemCount: _listings.length * 2,
+    return ListView.separated(
+        itemCount: _listings.length,
+        separatorBuilder: (context, i) => Divider(),
         itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-
-          final index = i ~/ 2;
-          if (index >= _listings.length) {
-            if (!fetching) {
+          if (i >= _listings.length) {
+            if (!fetching && res != 0) {
               _loadMoreListings();
-            }
-            if (res == 0) {
-              return Divider();
             }
           }
           return Container(
-              height: 300,
-              child: Column(children: [
-                Text(
-                  "Geese Movements #$index",
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Color(0xff2C1A1D),
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Image.asset(
-                  "lib/pics/${index % 6 + 1}.png",
-                  width: 200,
-                  height: 200,
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  "Some nice thic gooser don't ya think?",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Color(0xff2C1A1D),
-                      fontWeight: FontWeight.bold),
-                ),
-              ]));
+              height: 200,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: Image.asset(
+                              "lib/pics/${i % 6 + 1}.png",
+                              width: 200,
+                              height: 200,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          _listings[i].title,
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Color(0xff2C1A1D),
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "\$${_listings[i].price.toString()}",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xff2C1A1D),
+                          ),
+                        ),
+                      ]),
+                  (i + 1) >= _listings.length
+                      ? Container(
+                          width: 200,
+                        )
+                      : Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                              Expanded(
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  child: Image.asset(
+                                    "lib/pics/${(i + 1) % 6 + 1}.png",
+                                    width: 200,
+                                    height: 200,
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                _listings[i + 1].title,
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    color: Color(0xff2C1A1D),
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "\$${_listings[i + 1].price.toString()}",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Color(0xff2C1A1D),
+                                ),
+                              ),
+                            ]),
+                ],
+              ));
         });
   }
 
